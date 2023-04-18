0632f412722b6d8f983416efb673ac4d0b93f012
isaiah vang <vang.isaiah3@gmail.com>
2023-04-18 09:38:14 -0400

    finished making first name generator

--- src/main/java/com/iv/WorkingWithString.java
+++ src/main/java/com/iv/WorkingWithString.java
@@ -1,8 +1,89 @@
 package com.iv;
 
+import java.util.Arrays;
+
 public class WorkingWithString {
     public static void main(String[] args){
 
-        System.out.println("Probability Manipulation");
+//        String pp = "Peepee ";
+//        String popo = "Poopoo ";
+//        System.out.println(pp.concat(popo)); // .concat only works on Strings
+//        System.out.println(pp + popo);  // will look the same as above (+ and concat are the same)
+//        String result = pp.concat(popo);
+//
+//        int wordLength = result.length();
+//        System.out.println((wordLength));
+//        /////////////////////////////
+//        char myChar = 'G'; // single ' is char type
+//        String myName = "Greg"; // double " is String type
+//
+//        String phrase = "Big tex says \n \"Hello\"! test"; // \ means the next thing is not to be processed as code
+//        System.out.println(phrase);
+
+        ///////////
+
+//        int num1 = 10;
+//        int num2 = 10;
+//        System.out.println(num1 == num2);
+
+//        String phrase1 = new String ("Hello World");
+//        String phrase2 = new String ("Hello World");
+//        String phrase3 = "Hello " + "World";
+//        //Primitives (data type?): int long boolean short char double float // compare (==)
+//        // Non-primitive (class?): String // compare .equals
+//
+//        System.out.println(phrase1.equals(phrase2));
+
+        /////////////////////////
+
+//        String myName = "Isaiah";
+////        String myUpperCaseName = myName.toUpperCase(); //
+//        myName =myName.trim().toUpperCase();
+////        System.out.println(myUpperCaseName);
+//        System.out.println(myName);
+
+        //////////////////////
+
+//        String phrase = "              Hello World            !         ";
+//        System.out.println(phrase.trim());
+
+        //////////////
+
+//        String myPhrase = "Isaiah is the best programmer!";
+//        System.out.println(myPhrase.startsWith("isaiah"));
+
+//        String[] = ["Name: Isaiah", "From: USA", "Favorite Color: Periwinkle"]; // if you just wanted
+//        the name you use .startsWith
+
+        ///////////////
+
+//        String trackingCode = "USA-12981-Y-22";
+
+//        char hasShipped = trackingCode.charAt(10);
+//        System.out.println(hasShipped);
+
+        ///////////
+
+//        String productCode1 = "ACME-12213"; // "ACME", "12213"
+//        int dashPosition1 = productCode1.indexOf("-"); // 4
+//        String vendor1 = productCode1.substring(0, dashPosition1);
+//        String productNum1 = productCode1.substring(dashPosition1 + 1);
+//        System.out.println("Vendor1: " + vendor1 + " ProductNum1: " +productNum1);
+//
+//        String productCode2 = "SAMSUNG-320233242"; // "SAMSUNG" - "320233242"
+//        int dashPosition2 = productCode2.indexOf("-"); // 7
+//        String vendor2 = productCode2.substring(0, dashPosition2);
+//        String productNum2 = productCode2.substring(dashPosition2 + 1);
+//        System.out.println("Vendor2: " + vendor2 + " ProductNum2: " + productNum2);
+
+        ////////////////////////////
+
+//        String str1 = "my@dear@aunt@sally";
+//        System.out.println(str1.substring(3, 7));
+//
+//        String str2 = "my@dear@aunt@sally";
+//        System.out.println(Arrays.toString(str2.split("@")));
+
+        ////////////////////////////////
     }
 }
